369f9c7039cda8ce28c3b4d1f107a57a2315e3ce
martin <slowgore-martin@users.noreply.github.com>
2019-05-20 22:35:57 +0200

    changement MenuHaut

--- menuitem.cpp
+++ menuitem.cpp
@@ -2,11 +2,14 @@
 
 MenuItem::MenuItem(int vie)
 {
-    rectangleDuHaut = new QGraphicsRectItem(0, 0, 500,70);
+    rectangleDuHaut = new QGraphicsRectItem(0, 0, 100,70);
     for (int i = 0; i < vie; i++)
     {
         hearts.push_back(new QGraphicsPixmapItem(QPixmap(":/images/images/zelda.png").scaled(20,20)));
     }
+    rectangleDuHaut->setBrush(QColor(255,255,255));
+    //rectangleDuHaut->setPen(Qt::NoPen);
+    rectangleDuHaut->setOpacity(0.5);
 }
 
 QGraphicsRectItem* MenuItem::getRect(){
